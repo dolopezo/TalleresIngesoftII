7ca0ad413457eb290a817cdd0576f77f08b128a1
dolopezo <dolopezo@unal.edu.co>
2015-03-16 20:28:59 -0500

    -Añadida clase User

---
+++ src/User.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Familia López Ochoa on 16/03/2015.
+ */
+public class User {
+    String name;
+    String lastname;
+    int age;
+    String username;
+    String password;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getLastName(){
+        return lastname;
+    }
+
+    public void setLastName(String lastname){
+        this.lastname=lastname;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username=username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+
+}
